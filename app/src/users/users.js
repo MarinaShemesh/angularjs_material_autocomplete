5102077fc3360fd58bcee3633017b7ffcaa138e1
MarinaShemesh <marina.shemesh1@gmail.com>
2017-11-16 14:29:11 +0200

    16nov_2

--- app/src/users/users.js
+++ app/src/users/users.js
@@ -5,8 +5,8 @@
        .config(function($mdIconProvider, $mdThemingProvider){
          // Register the user `avatar` icons
             $mdIconProvider
-              .defaultIconSet("./assets/svg/avatars.svg", 128)
-              .icon("menu"       , "./assets/svg/menu.svg", 24);
+              .defaultIconSet("app/assets/svg/avatars.svg", 128)
+              .icon("menu", "app/assets/svg/menu.svg", 24);
               
               $mdThemingProvider.theme('default')
                           .primaryPalette('grey')
